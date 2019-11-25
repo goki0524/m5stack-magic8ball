7cef2bb54c139d316f25ef0c3bea32cba2250b5a
goki sakaguchi <goki0524@gmail.com>
2019-11-25 21:27:23 +0900

    Add IMU & Add shakeCheck function

--- magic8ball.ino
+++ magic8ball.ino
@@ -1,15 +1,29 @@
+#define M5STACK_200Q
+
 #include <HTTPClient.h>
 #include <M5Stack.h>
 #include <WiFi.h>
 #include <ArduinoJson.h>
-#include "utility/MPU6886.h"
+
+float accX = 0.0F;
+float accY = 0.0F;
+float accZ = 0.0F;
+float gyroX = 0.0F;
+float gyroY = 0.0F;
+float gyroZ = 0.0F;
+float pitch = 0.0F;
+float roll  = 0.0F;
+float yaw   = 0.0F;
+float temp = 0.0F;
+int MoPin = 26;  // vibrator Grove connected to digital pin 26
+
 
 // WiFi
 const char* WIFI_SSID = "WIFI_SSID"; 
 const char* WIFI_PASSWROD= "WIFI_PASSWROD";
 
 // WEB API 
-const String citycode = "citycode";
+const String citycode = "1252408";
 const String baseurl = "http://api.openweathermap.org/data/2.5/forecast";
 const String apikey = "apikey";
 
@@ -17,22 +31,11 @@ const String sunny = "Clear";
 const String cloudy = "Clouds";
 const String rainy = "Rain";
 
-// ja
-//const String sunnyArr[] = {"確実です。", "決定的にそうです。", "間違いありません。", "はい、確かに。", "当てにしていいです。", "私の知るところでは、イエスです。"};
-//const String cloudyArr[] = {"多分きっとそうです。", "幸先良好です。", "はい。", "兆候がいいです。", "答えははっきりしていません。もう一度試して下さい。", "後でまた尋ねて下さい。", "答えは今教えてあげない方がいいと思います。", "今のところ予測はつきません。", "精神を集中してもう一度尋ねて下さい。"};
-//const String rainyArr[] = {"当てにしてはいけません。", "私の答えは、ノーです。", "私の資料／情報源によるとダメだそうです。", "幸先はあまり良くありません。", "非常に怪しい／危なっかしいです。"};
-
-// en
-//const String sunnyArr[] = {"It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes"};
-//const String cloudyArr[] = {"Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again"};
-//const String rainyArr[] = {"Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
-
 const char* imgNameArr[] = {"/title.jpg", "/answer_01.jpg", "/answer_02.jpg", "/answer_03.jpg", "/answer_04.jpg", "/answer_05.jpg", "/answer_06.jpg", "/answer_07.jpg", "/answer_08.jpg", "/answer_09.jpg",
 "/answer_10.jpg", "/answer_11.jpg", "/answer_12.jpg", "/answer_13.jpg", "/answer_14.jpg", "/answer_15.jpg", "/answer_16.jpg", "/answer_17.jpg", "/answer_18.jpg", "/answer_19.jpg", "/answer_20.jpg"};
 
 unsigned int counter;
 
-MPU6886 IMU;
 
 void updateWeather() {
   
@@ -64,20 +67,42 @@ void updateWeather() {
   }
 }
 
+bool shakeCheck() {
+  M5.IMU.getGyroData(&gyroX,&gyroY,&gyroZ);
+  M5.IMU.getAccelData(&accX,&accY,&accZ);
+  M5.IMU.getAhrsData(&pitch,&roll,&yaw);
+  M5.IMU.getTempData(&temp);
+        
+  if ( abs(accX) > 1.2 && abs(accY) > 1.2 && abs(accZ) > 1.2) {
+    return true;
+    digitalWrite(MoPin, HIGH);
+  }
+  else {
+    digitalWrite(MoPin, LOW);
+  }
+  delay(1);
+  
+}
 
 
 void setup() {
-  // put your setup code here, to run once:
-  
-  M5.begin();
-  M5.Lcd.clear();
-  M5.Lcd.setTextSize(2);
 
-  // Font Setup
-//  M5.Lcd.setTextWrap(true, true);
-  // Font Japanese
-//  String fileName = "/Final-Frontier-28.vlw";
-//  M5.Lcd.loadFont(fileName, SD);
+  // Initialize the M5Stack object
+  M5.begin();
+   /*
+    Power chip connected to gpio21, gpio22, I2C device
+    Set battery charging voltage and current
+    If used battery, please call this function in your project
+  */
+  M5.Power.begin();
+
+  // Initialize IMU
+  M5.IMU.Init();
+  pinMode( MoPin, OUTPUT );
+
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(GREEN , BLACK);
+  M5.Lcd.setTextSize(2);
 
   // Display title image
   M5.Lcd.drawJpgFile(SD, imgNameArr[0], 0, 0);
@@ -93,16 +118,19 @@ void setup() {
   }
   M5.Lcd.println("...Connected!");
   delay(2000);
-  
-  M5.Lcd.fillScreen(0x3186);
+
   counter = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   if (counter == 0) {
+    M5.Lcd.println("Shake Start !!");
+    if (shakeCheck()) {
       updateWeather();
-    }
+     }
+  }
+  
   delay(5000);
 
 }
